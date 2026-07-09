dcd37a387715bb8028f8c2e463e16384ea476b02
yasminemhs22-sys <yasmine.mhs22@gmail.com>
2026-07-09 12:30:09 +0000

    Fix duplicate Link import

--- src/components/shop/ProductsTable.tsx
+++ src/components/shop/ProductsTable.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Trash2, Pencil } from 'lucide-react'
 import { createClient } from '@/lib/supabase/client'
