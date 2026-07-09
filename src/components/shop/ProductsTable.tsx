b01f96dbaf9a1d9abfd745a653504427bd9edb3e
yasminemhs22-sys <yasmine.mhs22@gmail.com>
2026-07-09 12:18:56 +0000

    Add product edit page

--- src/components/shop/ProductsTable.tsx
+++ src/components/shop/ProductsTable.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Trash2, Pencil } from 'lucide-react'
 import { createClient } from '@/lib/supabase/client'
